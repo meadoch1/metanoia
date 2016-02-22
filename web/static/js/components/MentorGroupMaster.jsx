bde3df908b7b7cf967c039afd10c6a49d2c271cb
Chris Meadows <meadoch1@gmail.com>
2016-02-21 21:59:52 -0500

    WIP:  MentorGroupMaster almost

--- web/static/js/components/MentorGroupMaster.jsx
+++ web/static/js/components/MentorGroupMaster.jsx
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import { shallowCompare } from 'react-addons-shallow-compare';
 import MentorGroupDetail from './MentorGroupDetail';
 import { Link } from 'react-router';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 
 export default class MentorGroupMaster extends Component {
   shouldComponentUpdate(nextProps, nextState) {
@@ -8,15 +10,16 @@ export default class MentorGroupMaster extends Component {
   }
 
   render() {
+    const group = this.props.group;
     return (
       <div className="panel panel-default" >
         <div className="panel-heading">
-          <h4 className="panel-title group-title"><strong>{this.props.name} ({this.props.assignments.length})  </strong>
-            <small>{this.props.title}</small>
+          <h4 className="panel-title group-title"><strong>{group.get("name")} ({group.get("mentor_group_assignments", List({})).length})  </strong>
+            <small>Fill in title somehow</small>
             <div className='pull-right'>
-              <Link to={`/mentoring/email_group/${this.props.id}`}><i className="fa fa-envelope" ></i></Link>
+              <Link to={`/mentoring/email_group/${group.get("id")}`}><i className="fa fa-envelope" ></i></Link>
                 &nbsp;
-              <Link to={`/mentoring/edit_group/${this.props.id}`}><i className="fa fa-edit" ></i></Link>
+              <Link to={`/mentoring/edit_group/${group.get("id")}`}><i className="fa fa-edit" ></i></Link>
             </div>
           </h4>
         </div>
@@ -30,9 +33,8 @@ export default class MentorGroupMaster extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.props.assignments.map((assignment, index) =>
-              <MentorGroupDetail {...assignment}
-                                 key={"assignment" + assignment.id} />
+            {group.get("mentor_group_assignments", List({})).map((assignment_id, index) =>
+              <MentorGroupDetail entities={this.props.entities} group_assignment={this.props.entities.getIn(["group_assignments", assignment_id.toString()], Map({}))} leader_id={group.get("leader_id").toString()}/>
             )}
           </tbody>
         </table>
@@ -42,15 +44,19 @@ export default class MentorGroupMaster extends Component {
 }
 
 MentorGroupMaster.propTypes = {
-  id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-//  onGroupReportClick: PropTypes.func.isRequired,
-  assignments: PropTypes.arrayOf(PropTypes.shape({
+  group: ImmutablePropTypes.contains({
+    schedule_entries: ImmutablePropTypes.list.isRequired,
+    name: PropTypes.string.isRequired,
+    mentor_group_assignments: ImmutablePropTypes.list.isRequired,
+    leader_id: PropTypes.number.isRequired,
     id: PropTypes.number.isRequired,
-    mentor_name: PropTypes.string.isRequired,
-    mentee_name: PropTypes.string,
-    facility_ref_cd: PropTypes.string,
-    comments: PropTypes.string
-  }).isRequired).isRequired
+    facility_id: PropTypes.number.isRequired,
+    facility: PropTypes.number.isRequired
+  }).isRequired,
+  entities: ImmutablePropTypes.contains({
+    persons: ImmutablePropTypes.map.isRequired,
+    clients: ImmutablePropTypes.map.isRequired,
+    volunteers: ImmutablePropTypes.map.isRequired,
+    group_assignments: ImmutablePropTypes.map.isRequired
+  }).isRequired
 }
